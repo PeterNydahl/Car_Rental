39785dc41946a78103d343da3242166bb8db24a2
Peter Nydahl <peter.nydahl@iths.se>
2023-10-14 18:43:58 +0200

    Version innan jag ändrar listorna till IEnumerable

--- Car_Rental.Common/Enums/VehicleStatuses.cs
+++ Car_Rental.Common/Enums/VehicleStatuses.cs
@@ -4,6 +4,6 @@ public enum VehicleStatuses
 {
     Booked = 1,
     Available = 2,
-    Open = 3, // eventuellt ej nödvändig
-    Closed = 4  // eventuellt ej nödvändig
+    Open = 3, 
+    Closed = 4  
 }
